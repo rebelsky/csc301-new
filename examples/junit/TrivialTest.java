f3c25760360e5f3d100ee60cce013a1e20ef82c8
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2021-08-30 13:07:55 -0500

    Add a JUnit test.

---
+++ examples/junit/TrivialTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class TrivialTest
+{
+  @Test
+  void trivialTest()
+  {
+    assertTrue();
+  } // trivialTest
+} // class TrivialTest
+
